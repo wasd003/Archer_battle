852d69a871502fcd2d366d8f711485d3ff2345cc
wasd003 <2206475938@qq.com>
2019-05-05 19:44:03 +0800

    Improve mobile mobility

--- AW.io/Classes/HelloWorldScene.cpp
+++ AW.io/Classes/HelloWorldScene.cpp
@@ -1,25 +1,25 @@
 #include "HelloWorldScene.h"
 #include "SimpleAudioEngine.h"
-
+#include "SingleGameScene.h"
+#include "MultiGameScene.h"
+#include "HelpScene.h"
+#include "StoreScene.h"
 USING_NS_CC;
-
+//创建场景
 Scene* HelloWorld::createScene()
 {
     return HelloWorld::create();
 }
 
-// Print useful error message instead of segfaulting when files are not there.
+// 打印异常
 static void problemLoading(const char* filename)
 {
     printf("Error while loading: %s\n", filename);
     printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
-// on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
     if ( !Scene::init() )
     {
         return false;
@@ -28,11 +28,7 @@ bool HelloWorld::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
+   //添加关闭菜单
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
                                            "CloseSelected.png",
@@ -50,19 +46,27 @@ bool HelloWorld::init()
         float y = origin.y + closeItem->getContentSize().height/2;
         closeItem->setPosition(Vec2(x,y));
     }
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
+	//添加四种功能菜单
+	auto SingleGameItem = MenuItemFont::create("Start SingleGame", CC_CALLBACK_1(HelloWorld::menuCallback, this));//单人游戏
+	auto MultiGameItem = MenuItemFont::create("Start MultiGame", CC_CALLBACK_1(HelloWorld::menuCallback, this));//多人游戏
+	auto HelpItem = MenuItemFont::create("Help", CC_CALLBACK_1(HelloWorld::menuCallback, this));//帮助
+	auto StoreItem = MenuItemFont::create("Store", CC_CALLBACK_1(HelloWorld::menuCallback, this));//商店
+	SingleGameItem->setPosition(Point(origin.x + visibleSize.width / 2 - closeItem->getContentSize().width / 2, 200));//为菜单设置位置
+	MultiGameItem->setPosition(Point(origin.x + visibleSize.width / 2 - closeItem->getContentSize().width / 2, 150));
+	HelpItem->setPosition(Point(origin.x + visibleSize.width / 2 - closeItem->getContentSize().width / 2, 100));
+	StoreItem->setPosition(Point(origin.x + visibleSize.width / 2 - closeItem->getContentSize().width / 2, 50));
+	SingleGameItem->setTag(SingleGameTag);//为菜单设置标签
+	MultiGameItem->setTag(MultiGameTag);
+	HelpItem->setTag(HelpTag);
+	StoreItem->setTag(StoreTag);
+	// 创建menu
+	auto menu = Menu::create(closeItem, SingleGameItem, MultiGameItem, HelpItem, StoreItem,NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+	//创建label 欢迎来到 《弓箭手大作战》
+    auto label = Label::createWithTTF("Welcome To Arrow War", "fonts/Marker Felt.ttf", 24);
+	label->setColor(Color3B::RED);
     if (label == nullptr)
     {
         problemLoading("'fonts/Marker Felt.ttf'");
@@ -77,8 +81,8 @@ bool HelloWorld::init()
         this->addChild(label, 1);
     }
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+   //创建初始场景的背景图片
+    auto sprite = Sprite::create("background.jpg");
     if (sprite == nullptr)
     {
         problemLoading("'HelloWorld.png'");
@@ -97,17 +101,26 @@ bool HelloWorld::init()
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-    //Close the cocos2d-x game scene and quit the application
     Director::getInstance()->end();
+}
 
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
-
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-
-
+void HelloWorld::menuCallback(Ref* pSender)
+{
+	auto NowItem = static_cast<MenuItem*>(pSender);
+	int tag = NowItem->getTag();
+	switch (tag)
+	{
+	case SingleGameTag:
+		Director::getInstance()->replaceScene(SingleGameScene::CreateScene());
+		break;
+	case MultiGameTag:
+		Director::getInstance()->replaceScene(MultiGameScene::CreateScene());
+		break;
+	case HelpTag:
+		Director::getInstance()->replaceScene(HelpScene::CreateScene());
+		break;
+	case StoreTag:
+		Director::getInstance()->replaceScene(StoreScene::CreateScene());
+		break;
+	}
 }
--- AW.io/Classes/HelloWorldScene.h
+++ AW.io/Classes/HelloWorldScene.h
@@ -12,9 +12,13 @@ public:
     
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
-    
+	void menuCallback(cocos2d::Ref*pSender);
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+	static const int SingleGameTag=10;//四种跳转场景的标签
+	static const int MultiGameTag=11;
+	static const int HelpTag=12;
+	static const int StoreTag=13;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
